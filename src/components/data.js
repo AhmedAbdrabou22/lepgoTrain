b72f9eccb84409ff9dbff023660694ec48e48baa
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2023-05-22 18:00:07 +0300

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,14 @@
+const data = [
+    {id:Math.random() , title:'وجبة افطار' , imgUrl: '1.jpg' , category:'افطار', discription:'هذه الوجبةاللذيذه ' , price:350},
+    {id:Math.random() , title:'وجبة غداء' , imgUrl: '2.jpg' , category:'غدا', discription:'هذه الوجبةاللذيذه ' , price:20},
+    {id:Math.random() , title:'وجبة عشاء' , imgUrl: '3.jpeg' , category:'عشا', discription:'هذه الوجبةاللذيذه ', price:150},
+    {id:Math.random() , title:'وجبة غداء ممتاز' , imgUrl: '1.jpg' , category:'غدا', discription:'هذه الوجبةاللذيذه ' , price:150},
+    {id:Math.random() , title:'وجبة افطار' , imgUrl: '1.jpg' , category:'افطار', discription:'هذه الوجبةاللذيذه ' , price:170},
+    {id:Math.random() , title:'وجبة غداء' , imgUrl: '3.jpeg' , category:'غدا', discription:'هذه الوجبةاللذيذه ' , price:20},
+    {id:Math.random() , title:'وجبة عشاء' , imgUrl: '1.jpg' , category:'عشا', discription:'هذه الوجبةاللذيذه ' , price:35},
+    {id:Math.random() , title:'وجبة افطار' , imgUrl: '3.jpeg' , category:'افطار', discription:'هذه الوجبةاللذيذه ' , price:179},
+    {id:Math.random() , title:'وجبة عشاء' , imgUrl: '1.jpg' , category:'عشا', discription:'هذه الوجبةاللذيذه ' , price:153},
+    {id:Math.random() , title:'وجبة غداء' , imgUrl: '2.jpg' , category:'غدا', discription:'هذه الوجبةاللذيذه ' , price:65},
+    {id:Math.random() , title:'وجبة سناكس' , imgUrl: '2.jpg' , category:'سناكس', discription:'هذه الوجبةاللذيذه ' , price:65},
+]
+export default data;
