2a7038d26b54457007cb07a46e3b8cfacec337a1
AhmedAbdrabou22 <ahmedabdrabou723@gmail.com>
2023-05-23 11:15:10 +0300

    Add Category Buttons and add data from data js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
+import { Container } from "react-bootstrap";
+import CategoryButton from "./components/categoryButton";
+import Foods from "./components/foods"
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Container>
+        <CategoryButton/>
+        <Foods/>
+      </Container>
     </div>
   );
 }
